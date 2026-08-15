5810d83f84b7a67d19ddeecf6f6b97c02ae3a5b3
haozhang86 <haozhang86@users.noreply.github.com>
2026-08-16 01:43:52 +0800

    refine request log status layout

--- web/console/assets/play-DKjy42Gv.js
+++ web/console/assets/play-DKjy42Gv.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-CNU-377s.js";/**
+import{c as o}from"./index-b7MKDAEl.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- web/console/assets/refresh-cw-CkbTMMeA.js
+++ web/console/assets/refresh-cw-CkbTMMeA.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CNU-377s.js";/**
+import{c as e}from"./index-b7MKDAEl.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
