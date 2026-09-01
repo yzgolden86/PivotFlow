f421356350401b381726d58ced2043c198bffae8
haozhang86 <haozhang86@users.noreply.github.com>
2026-09-01 14:58:30 +0800

    chore: rebuild console assets

--- web/console/assets/AnnouncementsPage-DIX6ulzS.js
+++ web/console/assets/AnnouncementsPage-DIX6ulzS.js
@@ -1,4 +1,4 @@
-import{c as Ga,Y as ju,j as F,r as ce,Z as Xa,_ as Lr,$ as Ka,a0 as $a,a1 as Ja,a2 as Za,a3 as es}from"./index-DqcBP8Lr.js";import{R as Or,O as Rr,L as ts,E as ns,a as rs,P as us,f as Vu}from"./shared-CYQyxKqU.js";import{s as An,M as is}from"./siteShared-DKpTg9BR.js";import{E as as}from"./external-link-nRQ1Ggse.js";/**
+import{c as Ga,Y as ju,j as F,r as ce,Z as Xa,_ as Lr,$ as Ka,a0 as $a,a1 as Ja,a2 as Za,a3 as es}from"./index-BWa1Vkm7.js";import{R as Or,O as Rr,L as ts,E as ns,a as rs,P as us,f as Vu}from"./shared-B5P_heHB.js";import{s as An,M as is}from"./siteShared-BmqQAsUr.js";import{E as as}from"./external-link-KAXWj-SR.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
